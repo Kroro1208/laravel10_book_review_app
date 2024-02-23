5b2b50bbe7d35340480f56f59a5385fd09277aa5
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-02-23 14:15:29 +0900

    各テーブル作成しDBで確認した

---
+++ database/migrations/2024_02_23_044515_create_reviews_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->text('review');
+            $table->unsignedInteger('rating'); // 正の整数およびゼロのみを格納できる数値型
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
