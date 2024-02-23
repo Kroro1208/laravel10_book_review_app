28df4e7ce68603f5c072bf5bf41dbee66872c00f
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-02-23 15:53:44 +0900

    BookとReviewそれぞれダミーデータを日本語で作成した

---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Review>
+ */
+class ReviewFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        //$faker = \Faker\Factory::create('ja_JP');
+        return [
+            'book_id' => null,
+            'review' => $this->faker->realText(200),
+            'rating' => fake()->numberBetween(1, 5),
+            'created_at' => fake()->dateTimeBetween('-2 years'),
+            'updated_at' => fake()->dateTimeBetween('created_at', now()),
+        ];
+    }
+
+    public function good()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'rating' => fake()->numberBetween(4, 5)
+            ];
+        });
+    }
+
+    public function average()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'rating' => fake()->numberBetween(2, 5)
+            ];
+        });
+    }
+
+    /**
+     * $this->state()はLaravelのファクトリクラス内で使用され、モデルインスタンスに対して特定の状態を定義する
+     * モデルの属性を変更するクロージャ（無名関数）を引数として受け取ることがポイント
+     *
+     *
+     */
+    //
+    public function bad()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'rating' => fake()->numberBetween(1, 3)
+            ];
+        });
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,20 +3,45 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Book;
+use App\Models\Review;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * 33件のBookインスタンスを生成し、各本に対して5から30の間でランダムに選ばれた数の
+     * 「良い」「平均」「悪い」評価を持つReviewインスタンスを関連付けて生成。
+     * つまり、各評価のレビュー本データが5～30で生成される。
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        Book::factory(33)->create()->each(function ($book) {
+            $numReviews = random_int(5, 30);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+            Review::factory()->count($numReviews)
+                ->good()
+                ->for($book) // for()は親モデルインスタンスに子モデルインスタンスを関連付ける
+                ->create();
+        });
+
+        Book::factory(33)->create()->each(function ($book) {
+            $numReviews = random_int(5, 30);
+
+            Review::factory()->count($numReviews)
+                ->average()
+                ->for($book) // for()は親モデルインスタンスに子モデルインスタンスを関連付ける
+                ->create();
+        });
+
+        Book::factory(33)->create()->each(function ($book) {
+            $numReviews = random_int(5, 30);
+
+            Review::factory()->count($numReviews)
+                ->bad()
+                ->for($book) // for()は親モデルインスタンスに子モデルインスタンスを関連付ける
+                ->create();
+        });
     }
 }
