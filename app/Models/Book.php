55f3cd9e2405eb712e814c90f735f4b5899854dc
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-02-23 18:45:58 +0900

    並び替えやフィルタリングに必要なメソッドを実装した

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Builder as QueryBuilder; // namsespaceに同じ名前は不可
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -23,15 +24,43 @@ class Book extends Model
 
     // レビューの数が多い順にソートする
     // 'reviews_count'はwithCount()により自動で作成される
-    public function scopePopular(Builder $query): Builder
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder | QueryBuilder
     {
-        return $query->withCount('reviews')->orderBy('reviews_count', 'desc');
+        return $query->withCount([
+            'reviews' => fn (Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ])->orderBy('reviews_count', 'desc');
     }
 
     // 評価が高い順にソートする
     // 'reviews_avg_rating'はwithAvg()により自動で作成される
-    public function scopeHighestRated(Builder $query): Builder
+    public function scopeHighestRated(Builder $query,  $from = null, $to = null): Builder | QueryBuilder
     {
-        return $query->withAvg('reviews', 'rating')->orderBy('reviews_avg_rating', 'desc');
+        return $query->withAvg([
+            'reviews' => fn (Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ], 'rating')->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    // having句が集計関数の結果に基づいてフィルタリングを行うSQLの特性があるのでwithCount('reviews')と一緒に使用する
+    // $books = Book::withCount('reviews')->minReviews(5)->get();
+    // 例えばこれは各書籍のレビュー数を計算し、レビュー数が5以上の書籍のみをフィルタリング可能
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder | QueryBuilder
+    {
+        return $query->having('reviews_count', '>=', $minReviews);
+    }
+
+
+    private function dateRangeFilter(Builder $query, $from = null, $to = null)
+    {
+        // レビューの作成日に基づいてフィルタリング
+        //$fromが指定されて、$toが指定されていない場合、$from以降に作成されたレビューのみをカウント
+        // $fromが指定なし、$toが指定されている場合、$to以前に作成されたレビューのみをカウント
+        // $fromと$toの両方が指定されている場合、その期間内に作成されたレビューのみをカウント
+        if ($from && !$to) {
+            $query->where('created_at', '>=', $from);
+        } elseif (!$from && $to) {
+            $query->where('created_at', '<=', $to);
+        } elseif ($from && $to) {
+            $query->whereBetween('created_at', [$from, $to]);
+        }
     }
 }
