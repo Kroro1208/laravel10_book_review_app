ed7d04c69b9c06f4b52873d474947624272c9580
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-02-26 08:55:02 +0900

    ボタン修正

--- resources/views/books/reviews/create.blade.php
+++ resources/views/books/reviews/create.blade.php
@@ -16,7 +16,9 @@
     <button type="submit" class="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-xl text-sm w-25 px-4 py-2.5 text-center">レビューを送る</button>
 </form>
 <div class="mt-5">
-    <button class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-xl text-sm w-20 px-4 py-2 text-center dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"><a href="{{route('books.show', $book)}}">戻る</a></button>
+    <button class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-xl text-sm w-20 px-4 py-2 text-center dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">
+        <a href="{{route('books.show', $book)}}">戻る</a>
+    </button>
 </div>
 
 @endsection
